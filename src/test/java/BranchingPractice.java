605cd6341322df52b5ad6eb84bd267a6a1256f93
UmidMahmudzade <umahmudzade@gmail.com>
2021-05-25 16:23:59 -0700

    Shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,13 @@
 public class BranchingPractice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
+        //We created new project
+        //enabled version control
+        //added .gitignore file
+        //made initial commit
+        //and shared in github.com
+
 
     }
 }
